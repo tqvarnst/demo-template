3d33afc23ea65458bddaf438f2c7ede44006cc71
tqvarnst <tqvarnst@redhat.com>
2014-04-29 10:47:11 +0200

    Neutralized the demo template for other products than BRMS

--- support/createDemo.sh
+++ support/createDemo.sh
@@ -81,148 +81,79 @@ echo ".metadata" > projects/.gitignore
 
 echo 
 echo Create init.sh for demo, named example_init.sh.
-echo '#!/bin/sh 
-DEMO="[insert-name] Demo"
-JBOSS_HOME=./target/jboss-eap-6.0
-SERVER_DIR=$JBOSS_HOME/standalone/deployments/
-SERVER_CONF=$JBOSS_HOME/standalone/configuration/
-LIB_DIR=./support/lib
-SRC_DIR=./installs
-EAP=jboss-eap-6.0.1.zip
-BRMS=brms-p-5.3.1.GA-deployable-ee6.zip
-EAP_REPO=jboss-eap-6.0.1-maven-repository
-VERSION=5.3.1
 
+cat > example_init.sh << EOF
+#!/bin/sh 
+DEMO="$1 Demo"
+#TODO: Add zip file(s) that should be unpacked as part of the installation
+ZIP_FILES=""
+INSTALL_DIR=./target
 
-echo
-echo "Setting up the JBoss Enterprise EAP 6 ${DEMO} environment..."
-echo
+pre_install () {
+	echo "Running pre install tasks"
+	check_zip_files_present
+	create_install_dir
+	#TODO: Add your custom pre_install steps here
+}
 
-# make some checks first before proceeding.	
-if [[ -r $SRC_DIR/$EAP || -L $SRC_DIR/$EAP ]]; then
-		echo EAP sources are present...
+install () {
+	for zip in \${ZIP_FILES}
+	do
+		echo "Unpacking \$zip"
+		unzip -q -d \${INSTALL_DIR} \$zip
+	done
+}
+
+post_install () {
+	echo "Running post install tasks"
+	#TODO: Add your custom post_install steps here
+}
+
+###### Util functions
+abort_with_error_msg () {
+	echo "[ERROR] Installation has aborted with the following message: \$1"
+	exit 99
+}
+
+check_zip_files_present () {
+	for zip in \${ZIP_FILES}
+	do
+		if [ ! -f \${zip} ]; then
+			abort_with_error_msg "Missing zip file \$zip, please make sure that you read the README.md file and downloaded the necessary zip files"  
+		fi
+	done
+}
+
+clean_if_not_empty () {
+	if [ "\$(ls -A \$1)" ]; then
+		rm -rf \$1/*
+	fi
+}
+
+create_install_dir () {
+	# Create the target directory if it does not already exist.
+	if [ ! -x \${INSTALL_DIR} ]; then
+		echo "  - creating the installation directory..."
 		echo
-else
-		echo Need to download $EAP package from the Customer Support Portal 
-		echo and place it in the $SRC_DIR directory to proceed...
+		mkdir \${INSTALL_DIR}
+	else
+		echo "  - installation directory exists, moving on..."
 		echo
-		exit
-fi
+		clean_if_not_empty \${INSTALL_DIR}
+	fi
+}
 
-# Create the target directory if it does not already exist.
-if [ ! -x target ]; then
-		echo "  - creating the target directory..."
-		echo
-		mkdir target
-else
-		echo "  - detected target directory, moving on..."
-		echo
-fi
-
-# Move the old JBoss instance, if it exists, to the OLD position.
-if [ -x $JBOSS_HOME ]; then
-		echo "  - existing JBoss Enterprise EAP 6 detected..."
-		echo
-		echo "  - moving existing JBoss Enterprise EAP 6 aside..."
-		echo
-		rm -rf $JBOSS_HOME.OLD
-		mv $JBOSS_HOME $JBOSS_HOME.OLD
-
-		# Unzip the JBoss EAP instance.
-		echo Unpacking JBoss Enterprise EAP 6...
-		echo
-		unzip -q -d target $SRC_DIR/$EAP
-else
-		# Unzip the JBoss EAP instance.
-		echo Unpacking new JBoss Enterprise EAP 6...
-		echo
-		unzip -q -d target $SRC_DIR/$EAP
-fi
-
-# Unzip the required files from JBoss BRMS Deployable
-echo Unpacking JBoss Enterprise BRMS $VERSION...
 echo
-cd installs
-unzip -q $BRMS
-
-echo "  - deploying JBoss Enterprise BRMS Manager WAR..."
+echo "Setting up the ${DEMO} environment..."
 echo
-unzip -q -d ../$SERVER_DIR jboss-brms-manager-ee6.zip
-rm jboss-brms-manager-ee6.zip 
 
-echo "  - deploying jBPM Console WARs..."
+pre_install
+install
+post_install
+
+echo "\${DEMO} Setup Complete."
 echo
-unzip -q -d ../$SERVER_DIR jboss-jbpm-console-ee6.zip
-rm jboss-jbpm-console-ee6.zip
-
-unzip -q jboss-jbpm-engine.zip 
-echo "  - copying jBPM client JARs..."
-echo
-unzip -q -d ../$SERVER_DIR jboss-jbpm-engine.zip lib/netty.jar
-cp -r lib ../$LIB_DIR
-cp jbpm-test-5.3.1.BRMS.jar ../$LIB_DIR
-cp jbpm-human-task-5.3.1.BRMS.jar ../$LIB_DIR
-cp jbpm-persistence-jpa-5.3.1.BRMS.jar ../$LIB_DIR
-cp jbpm-workitems-5.3.1.BRMS.jar ../$LIB_DIR
-rm jboss-jbpm-engine.zip
-rm -rf *.jar modeshape.zip *.RSA lib
-rm jboss-brms-engine.zip
-
-echo Rounding up, setting permissions and copying support files...
-echo
-cd ../
-
-echo "  - enabling demo accounts logins in brms-users.properties file..."
-echo
-cp support/brms-users.properties $SERVER_CONF
-
-echo "  - enabling demo accounts role setup in brms-roles.properties file..."
-echo
-cp support/brms-roles.properties $SERVER_CONF
-
-echo "  - adding dodeploy files to deploy all brms components..."
-echo 
-touch $SERVER_DIR/business-central-server.war.dodeploy
-touch $SERVER_DIR/business-central.war.dodeploy
-touch $SERVER_DIR/designer.war.dodeploy
-touch $SERVER_DIR/jboss-brms.war.dodeploy
-touch $SERVER_DIR/jbpm-human-task.war.dodeploy
-
-echo "  - configuring security authentication, copying updated components.xml file to jboss-brms.war..."
-echo
-cp support/components.xml $SERVER_DIR/jboss-brms.war/WEB-INF/
-
-echo "  - configuring deployment timeout extention and added security domain brms in standalone.xml..."
-echo
-cp support/standalone.xml $SERVER_CONF
-
-# Add execute permissions to the standalone.sh script.
-echo "  - making sure standalone.sh for server is executable..."
-echo
-chmod u+x $JBOSS_HOME/bin/standalone.sh
-
-echo "  - enabling demo users for human tasks in jbpm-human-task.war web.xml file..."
-echo
-cp support/jbpm-human-task-war-web.xml $SERVER_DIR/jbpm-human-task.war/WEB-INF/web.xml
-
-echo "  - enabling work items by registering Email and Log nodes..."
-echo
-cp support/drools.session.conf $SERVER_DIR/business-central-server.war/WEB-INF/classes/META-INF
-cp support/CustomWorkItemHandlers.conf $SERVER_DIR/business-central-server.war/WEB-INF/classes/META-INF
-chmod 644 $SERVER_DIR/business-central-server.war/WEB-INF/classes/META-INF/drools.session.conf
-chmod 644 $SERVER_DIR/business-central-server.war/WEB-INF/classes/META-INF/CustomWorkItemHandlers.conf
-
-echo "  - adding model jar to business central admin console classpath..." 
-echo
-cp support/customereval-model.jar $SERVER_DIR/business-central-server.war/WEB-INF/lib
-
-echo "  - adding netty dep to business-central-server.war and jbpm-human-task.war..."
-echo
-cp support/MANIFEST.MF $SERVER_DIR/business-central-server.war/WEB-INF/classes/META-INF/
-cp support/MANIFEST.MF $SERVER_DIR/jbpm-human-task.war/WEB-INF/classes/META-INF/
-
-echo "JBoss Enterprise BRMS ${VERSION} ${DEMO} Setup Complete."
-echo' > example_init.sh
+EOF
 
 echo
 echo You can new view your project directory setup in $1.
